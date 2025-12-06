df08dbd0486619b682a423ed9affb2025b4e3485
Jules Kreuer <jules.kreuer@uni-tuebingen.de>
2025-12-06 14:51:09 +0100

    typo

--- analysis/grouping_benchmark.py
+++ analysis/grouping_benchmark.py
@@ -1,9 +1,9 @@
+import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import pandas as pd
 import numpy as np
-import os
 import glob
 from scipy.io import loadmat
 from sklearn.utils import shuffle
